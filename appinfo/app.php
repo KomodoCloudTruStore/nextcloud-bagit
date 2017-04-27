58a36b639d4f54bd06c3db367dd537c95d749f26
maxwelllincoln <me@maxwelllincoln.com>
2017-04-27 09:59:12 -0500

    update app icon

--- appinfo/app.php
+++ appinfo/app.php
@@ -7,7 +7,7 @@
         'id' => 'bagit',
         'order' => 10,
         'href' => $urlGenerator->linkToRoute('bagit.view.index'),
-        'icon' => $urlGenerator->imagePath('bagit', 'app.svg'),
+        'icon' => $urlGenerator->imagePath('bagit', 'bagit-black.svg'),
         'name' => \OC::$server->getL10N('bagit')->t('BagIt'),
     ];
 
