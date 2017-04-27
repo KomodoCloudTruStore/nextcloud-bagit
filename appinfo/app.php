cc9c2553ba2fd44d41a3a4d51f7385f4f9dde09f
maxwelllincoln <me@maxwelllincoln.com>
2017-04-27 10:37:43 -0500

    fixed icons

--- appinfo/app.php
+++ appinfo/app.php
@@ -7,7 +7,7 @@
         'id' => 'bagit',
         'order' => 10,
         'href' => $urlGenerator->linkToRoute('bagit.view.index'),
-        'icon' => $urlGenerator->imagePath('bagit', 'bagit-black.svg'),
+        'icon' => $urlGenerator->imagePath('bagit', 'app.svg'),
         'name' => \OC::$server->getL10N('bagit')->t('BagIt'),
     ];
 
